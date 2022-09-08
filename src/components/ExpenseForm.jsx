2078f734ff124f663ca7a7a11b58f224b0375436
GytAndr <andriejunas@gmail.com>
2022-09-08 15:54:08 +0300

    categories hooked up

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { addExpense, updateReceiptCategory } from "../store/receiptSlice";
+import Categories from "./Categories";
 function ExpenseForm({ receiptId }) {
 	const dispatch = useDispatch();
-	const onReceiptCategoryChange = (event) => {
+	const onReceiptCategoryChange = (value) => {
 		const payload = {
 			receiptId: receiptId,
-			value: event.target.value,
+			value: value,
 		};
 		dispatch(updateReceiptCategory(payload));
 	};
+	const countryList = [
+		{ id: 1, name: "Food" },
+		{ id: 2, name: "Houseware" },
+		{ id: 3, name: "Entertainment" },
+	];
+	const defaultSelectText = "Select:";
 	return (
 		<div>
+			<Categories
+				defaultText={defaultSelectText}
+				optionsList={countryList}
+				onReceiptCategoryChange={onReceiptCategoryChange}
+			/>
 			<button
 				onClick={() => dispatch(addExpense(receiptId))}
 				className="btn-add-expense"
